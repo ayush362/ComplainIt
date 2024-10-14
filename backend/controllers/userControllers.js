4443e5a8b3da736cbd1bed7c84ca1fff65d16c1c
ayush362 <ayushkhatri362@gmail.com>
2024-10-14 09:17:32 +0530

    Backend setup done

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,42 @@
+import asyncHandler from "express-async-handler";
+// @desc       Auth user/set token
+// route       POST /API/USERS/auth
+// @access      public
+const authUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "Auth User" });
+});
+
+// @desc       Register a n ew user
+// route       POST /API/USERS
+// @access      public
+const registerUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "Register User" });
+});
+
+// @desc       Logout User
+// route       POST /API/USERS/logout
+// @access      public
+const logoutUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "Logout User" });
+});
+
+// @desc       Get User Profile
+// route       GET /API/USERS/profile
+// @access      private
+const getUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: " User profile" });
+});
+
+// @desc       Update User profile
+// route       PUT /API/USERS/profile
+// @access      private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "Update User Profile" });
+});
+export {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile,
+};
